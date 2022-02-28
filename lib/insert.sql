4184be87c8406b6e3fcf1629374bd9d4ed0ecf41
Matt Draghi <mtd5264@gmail.com>
2022-02-28 15:40:05 -0500

    commit tree f1b4a484f587c3466875c1a18242251f001207e8

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "Not bad", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "brown", "meow", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 56, "F", "white", "happy", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 13, "M", "brown", null, 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 100, "F","black", "woof", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 3333, "F", "green", "GRRR", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "F", "brown", null, 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (null, 34, "M", "blue", "grrr", 1);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,71 @@
 def selects_all_female_bears_return_name_and_age
   "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name,
+      bears.age
+    FROM
+      bears
+    WHERE
+      sex="F";
+  SQL
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name
+    FROM
+      bears
+    ORDER BY
+      bears.name;
+  SQL
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
+  <<-SQL
+    select
+    bears.name,
+    bears.age
+    from
+    bears
+    where
+    alive = 1
+    order by
+    age
+    asc;
+  SQL
 end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  <<-SQL
+    select
+    bears.name,
+    bears.age
+    from 
+    bears
+    order by 
+    age 
+    desc
+    limit 1;
+  SQL
 end
 
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  <<-SQL
+    select
+      bears.name,
+      bears.age
+    from
+      bears
+    order by
+      age
+      asc
+    limit
+      1;
+  SQL
 end
